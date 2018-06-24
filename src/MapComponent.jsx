179970d11d85095c354dfdab53e5c6aabe0c0ff1
MaxGrigorev@github.com <MaxGrigorev@github.com>
2018-06-25 02:13:26 +0500

    graggable

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -11,14 +11,22 @@ export class MapContainer extends Component {
 	state = {
 		activeMarker: {},
 		selectedPlace: {},
-		showingInfoWindow: false
+		showingInfoWindow: false,
+		positionM:67.439192
+
 	};
 	render() {
 		return (
-				<Map google={ this.props.google} zoom={ 14 } onClick={this.onMapClicked}>
+				<Map google={ this.props.google} zoom={ 14 } onClick={this.onMapClicked} initialCenter={{
+            lat: 40.854885,
+            lng: -88.081807
+          }} onDragend={this.centerMoved}>
 
 					<Marker onClick = {this.onMarkerClick}
 					name = {'Current location'}
+					draggable={true}
+					position={{lat: this.state.positionM, lng: -122.439192}}
+					onDragend={this.markerDraggen}
 					/>
 
 				<InfoWindow
@@ -35,7 +43,7 @@ export class MapContainer extends Component {
 		);
 	}
 	onMarkerClick = (props, marker) => {
-			console.log("onMarkerClick")
+			console.log("onMarkerClick", marker.position.lng())
 			this.setState({
 				activeMarker: marker,
 				selectedPlace: props,
@@ -53,6 +61,15 @@ export class MapContainer extends Component {
 			showingInfoWindow: false
 		});
 	};
+
+	centerMoved(mapProps, map) {
+	  console.log("centerMoved", map.center.lng())
+	}
+
+
+	markerDraggen(prop,marker) {
+		console.log("markerDraggen", marker.position.lng())
+	}
 }
 export default GoogleApiWrapper({
 	apiKey: ('AIzaSyCCMgV6rNeRGouUp7vlhgZMC2n9mzA_Ltc')
